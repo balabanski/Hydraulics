a711205aaa09e81e03d234c3fddaba22ec7debe8
IVAN <balabanski.v@gmail.com>
2023-03-21 13:31:07 +0200

    create pump and parameters  file for PUMP

---
+++ Pump/parameters.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+from utils.parameters import file_name_input, r_from_file_to_metadata,\
+                            w_metadata_to_file, parameter_input
+
+metadata_pump = {}
+
+name_par_pump = {
+    "V": 'V (см3)- рабочий объём',
+    "n": "n (об/мин)- скорость вращения ",
+    "Q": "Q (л/мин)- требуемая подача",
+    "M": 'M (даН*м ) - (деканьютон*м)вращающий момент ведомого вала',
+    "P": 'P (кВт) - мощность  ведомого вала',
+    "p": 'p (Bar) - давление (перепад давления)',
+}
+
+
+initial_dir_mot = str(Path(Path.cwd(), 'Pump', 'JsonFiles'))
+#открываю или создаю файл для хранения параметров
+file_name_pump = file_name_input(initial_dir_mot, metadata= metadata_pump)
+
+
+
+# экземпляр функции r_from_file_to_metadata
+r_from_file = r_from_file_to_metadata(path_file=file_name_pump)
+
+
+#переопределяю переменную-получаю словарь с внешнего файла
+metadata_pump = r_from_file()
+
+# экземпляр функции w_metadata_to_file
+w_to_file_pump = w_metadata_to_file(path_file=file_name_pump, metadata= metadata_pump)
+
+parameter_pump_input = parameter_input(metadata = metadata_pump,
+                                    _name_par = name_par_pump,
+                                    _func_write = w_to_file_pump
+                                    )
--- Pump/pump.py
+++ Pump/pump.py
@@ -1,11 +1,15 @@
 
+# рабочий объём V(см3)
+def _V(Q, n= 1500, n_ob=0.95 ):
+    _V= Q * 1000 / (n * n_ob)
+    return _V
 
-# подача насоса Q(л/мин)_V(см3)_n(об/мин)
-def Q(V, n, n_ob=0.95):
-    Q= V * n * n_ob / 1000
-    return Q
+# подача насоса Q(л/мин)__n(об/мин)
+def _Q(V, n, n_ob=0.95):
+    _Q= V * n * n_ob / 1000
+    return _Q
 
 # мощность привода P(кВт)
-def P(p, Q, n_kpd=0.82):
-    P=p * Q /(600 * n_kpd)
-    return P
+def _P(p, Q, n_kpd=0.82):
+    _P=p * Q /(600 * n_kpd)
+    return _P
